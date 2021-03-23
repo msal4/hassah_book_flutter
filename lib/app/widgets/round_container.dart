b2432ba4c0c4f3aaa316e2c1e69af102f04cda79
Mohammed Salman <msal4@outlook.com>
2021-03-23 06:43:39 +0300

    add padding as an optional parameter to the RoundContainer constructor

--- lib/app/widgets/round_container.dart
+++ lib/app/widgets/round_container.dart
@@ -2,18 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:hassah_book_flutter/common/utils/const.dart';
 
 class RoundContainer extends StatelessWidget {
-  const RoundContainer(
-      {Key key, this.child, this.color = const Color(0xFFF5F5F5)})
-      : super(key: key);
+  const RoundContainer({
+    Key key,
+    this.child,
+    this.color = const Color(0xFFF5F5F5),
+    this.padding = const EdgeInsets.symmetric(
+      horizontal: kDefaultPadding * 1.5,
+      vertical: kDefaultPadding / 1.5,
+    ),
+  }) : super(key: key);
 
   final Widget child;
   final Color color;
+  final EdgeInsetsGeometry padding;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(
-          horizontal: kDefaultPadding * 1.5, vertical: kDefaultPadding / 1.5),
+      padding: padding,
       decoration: BoxDecoration(
         color: color,
         borderRadius: BorderRadius.circular(kDefaultRadius),
