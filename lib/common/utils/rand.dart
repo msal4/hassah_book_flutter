129f969ef252ef10ba373acb41fc79d33fb9d140
Mohammed Salman <msal4@outlook.com>
2021-04-09 16:25:18 +0300

    use a regular random number generator without cryptography (Closes #23)

--- lib/common/utils/rand.dart
+++ lib/common/utils/rand.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'dart:math';
 
 String generateRandomString([len = 5]) {
-  var random = Random.secure();
+  var random = Random();
   var values = List<int>.generate(len, (i) => random.nextInt(255));
   return base64UrlEncode(values);
 }
