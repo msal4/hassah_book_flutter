994ec448bbc23c0a601affc54aede9e89530b86a
Mohammed Salman <msal4@outlook.com>
2021-10-30 19:42:18 +0300

    Add android icons

--- android/app/src/main/kotlin/app/hassah/book/hassah_book_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/app/hassah/book/hassah_book_flutter/MainActivity.kt
@@ -2,5 +2,4 @@ package app.hassah.book.hassah_book_flutter
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity() {}
